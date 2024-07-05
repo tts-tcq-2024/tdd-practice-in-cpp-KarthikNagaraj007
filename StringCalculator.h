22e158218ce4f1c8b76b576a2e47b7edccd340e0
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-07-05 13:56:04 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,18 +2,17 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
-#include <vector>
 
 class StringCalculator {
 public:
     int add(const std::string& numbers);
 
 private:
-    std::vector<std::string> split(const std::string& s, const std::string& delimiters);
-    std::vector<int> extractNumbers(const std::string& numbers, const std::string& delimiters);
-    void checkForNegatives(const std::vector<int>& nums);
+    int sumTwoNumbers(int a, int b);
+    void checkNegative(int num);
+    int convertStringToInt(const std::string& str);
+    std::string handleCustomDelimiter(const std::string& numbers);
     std::string getDelimiters(const std::string& numbers);
-    std::string removeDelimiterSyntax(const std::string& numbers);
 };
 
 #endif // STRINGCALCULATOR_H
